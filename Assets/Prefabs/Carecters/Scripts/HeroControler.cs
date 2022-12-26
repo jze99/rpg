78609702b17a065ee4e0f39b03e48ab78f74818d
jze99 <jze99@mail.ru>
2022-12-27 01:38:04 +0500

    start

---
+++ Assets/Prefabs/Carecters/Scripts/HeroControler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HeroControler : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public float spead;
+    public float distans;
+    public Vector3 target;
+    public string act;
+    void Start()
+    {
+        agent =GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        switch(act)
+        {
+            case "Move": 
+                Move();
+            break;
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hit, 100f))
+            {
+                ClickUpdate(hit);
+            }
+        }
+    }
+    void ClickUpdate(RaycastHit _hit)
+    {
+        if(_hit.transform.tag=="Ground")
+        {
+            target=_hit.point;
+            act="Move";
+        }
+    }
+    void Move()
+    {
+        distans=Vector3.Distance(transform.position,target);
+        anim.SetFloat("Spead",spead);
+        spead=Mathf.Clamp(spead,0,1);
+        if(distans>0.5f)
+        {
+            agent.SetDestination(target);
+            agent.isStopped=false;
+            spead+=2*Time.deltaTime;
+            anim.SetBool("Walk",true);
+        }
+        else if(distans<=0.5f)
+        {
+            spead-=5*Time.deltaTime;  
+            if(spead<=0.2f)
+            {
+                anim.SetBool("Walk",false);
+                agent.isStopped=true;
+                act="";
+            }
+        }
+    }
+}
---
+++ Assets/Prefabs/Inventory/Script/Item.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string nameItem;
+    public string typeItem;
+    public int prise;
+    public int mass;
+    public string pathSprite;
+    public string pathPrefab;
+}
